b37ae8e07b67bd48975934631495e39bfffafa5b
Guillermo Alvarado - Sentinel.la <guillermo@sentinel.la>
2019-07-31 16:20:40 -0500

    Network creation adjusted

--- openshift/roles/master/main.tf
+++ openshift/roles/master/main.tf
@@ -22,8 +22,7 @@ module "create_nova_instance_with_volume1" {
   instance_name = "ocp-master01"
   image_id = "9523d0ea-9ec4-447e-a71e-8be2c74a1228"
   flavor_id = "5bfcf6ad-0da9-4549-88be-2e377dbb67c0"
-  network_name1 = "39052a40-7a9a-40b8-b420-5fa8783b85c6"
-  network_name2 = "918fdcf8-ef1d-4c19-826d-c18229ce3cea"
+  network_name = "918fdcf8-ef1d-4c19-826d-c18229ce3cea"
   extra_volume_size = 100
 }
 
@@ -32,8 +31,7 @@ module "create_nova_instance_with_volume2" {
   instance_name = "ocp-master02"
   image_id = "9523d0ea-9ec4-447e-a71e-8be2c74a1228"
   flavor_id = "5bfcf6ad-0da9-4549-88be-2e377dbb67c0"
-  network_name1 = "39052a40-7a9a-40b8-b420-5fa8783b85c6"
-  network_name2 = "918fdcf8-ef1d-4c19-826d-c18229ce3cea"
+  network_name = "918fdcf8-ef1d-4c19-826d-c18229ce3cea"
   extra_volume_size = 100
 }
 
@@ -42,7 +40,7 @@ module "create_nova_instance_with_volume3" {
   instance_name = "ocp-master03"
   image_id = "9523d0ea-9ec4-447e-a71e-8be2c74a1228"
   flavor_id = "5bfcf6ad-0da9-4549-88be-2e377dbb67c0"
-  network_name = "39052a40-7a9a-40b8-b420-5fa8783b85c6"
+  network_name = "918fdcf8-ef1d-4c19-826d-c18229ce3cea"
   extra_volume_size = 100
 }
 
