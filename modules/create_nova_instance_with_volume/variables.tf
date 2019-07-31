04e2f949a4aaad0250f5921ecbb475ee538d3f96
Guillermo Alvarado - Sentinel.la <guillermo@sentinel.la>
2019-07-30 22:25:27 -0500

    Adding extra block device

--- modules/create_nova_instance_with_volume/variables.tf
+++ modules/create_nova_instance_with_volume/variables.tf
@@ -3,3 +3,4 @@ variable "flavor_id" {}
 variable "image_id" {}
 variable "network_name1" {}
 variable "network_name2" {}
+variable "extra_volume_size" {}
--- openshift/roles/master/variables.tf
+++ openshift/roles/master/variables.tf
@@ -3,4 +3,3 @@ variable "tenant_name" {}
 variable "password" {}
 variable "auth_url" {}
 variable "region" {}
-variable "extra_volume_size" {}
